305b49271b0c479b82804346689aee7e25928e0c
JhonatanFulguera <arthurdensek@gmail.com>
2022-01-09 07:56:29 -0300

    Aplicando Express Validator al formulario de Registro

--- src/routes/userRutas.js
+++ src/routes/userRutas.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-
+const upload = require('../middleware/middlewareMulterUsers')
+let validacion = require("../middleware/validatorUsers")
 
 
 router.get('/login', userController.login)
 router.get('/register', userController.register)
+router.post('/register',upload.single("image"),validacion,userController.processRegister)
 
 module.exports = router;
