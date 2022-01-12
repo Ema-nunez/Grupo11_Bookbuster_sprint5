1099ca040447de9585d4689f811e39a7a1c73087
MatiasAguirre26 <matiasezeaguirre@gmail.com>
2022-01-12 20:21:01 -0300

    Agrego el guestMiddleware, una nueva imagen para un avatar. Ccambios en userRutas, y agrego mi usuario MatiasAguirre

--- src/routes/userRutas.js
+++ src/routes/userRutas.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const upload = require('../middleware/middlewareMulterUsers')
 let validacion = require("../middleware/validatorUsers")
+const guestMiddleware = require('../middleware/guestMiddleware')
 
 
-router.get('/login', userController.login)
+//Formulario de Login
+router.get('/login', guestMiddleware ,userController.login)
+
+//Procesar el Login
 router.post('/login',userController.loginproceso)
-router.get('/register', userController.register)
+
+//Formulario de Registro
+router.get('/register', guestMiddleware ,userController.register)
+
+//Procesar el Registro
 router.post('/register',upload.single("image"),validacion,userController.processRegister);
+
 router.get('/logout',userController.logout)
 
 module.exports = router;
