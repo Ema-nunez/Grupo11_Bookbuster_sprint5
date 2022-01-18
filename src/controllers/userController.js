3ee472df1c63ee96ef383b99606c3372f6a436fc
Nacamino <nataliacam885@gmail.com>
2022-01-18 00:15:14 -0300

    remember me ready

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,83 +1,85 @@
 const { localsName } = require("ejs");
-const { validationResult } = require("express-validator")
-const bcrypt = require('bcrypt');
-const modelController = require('../model/jsonDatabase');
-const jsonDB = require('../model/jsonDatabase');
-const usersModel = jsonDB('users')
-
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const modelController = require("../model/jsonDatabase");
+const jsonDB = require("../model/jsonDatabase");
+const usersModel = jsonDB("users");
 
 const userController = {
-    login : (req,res)=>{
-        
-        return res.render('users/login')
-    },
+  login: (req, res) => {
+    return res.render("users/login");
+  },
 
-    loginproceso : (req,res)=>{
+  loginproceso: (req, res) => {
+    let userToLogin = usersModel.findByField("email", req.body.email);
 
-        let userToLogin = usersModel.findByField('email',req.body.email)
-        
-
-        if(userToLogin){
-            let passwordOk = bcrypt.compareSync(req.body.password, userToLogin.password)
-            if(passwordOk){
-                delete userToLogin.password;
-                delete userToLogin.confirmarPassword;
-                req.session.userLoged = userToLogin
-                return res.redirect('/')
-            }
-
-            return res.render('users/login',{
-                errors : {
-                    email : {
-                        msg : 'Las credenciales son invalidas'
-                    }
-                }
-            })
-
-            
+    if (userToLogin) {
+      let passwordOk = bcrypt.compareSync(
+        req.body.password,
+        userToLogin.password
+      );
+      if (passwordOk) {
+        delete userToLogin.password;
+        delete userToLogin.confirmarPassword;
+        req.session.userLoged = userToLogin;
+        if (req.body.recordarme) {
+          res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 * 3 });
         }
 
-        return res.render('users/login',{
-            errors : {
-                email : {
-                    msg : 'Este correo no esta registrado'
-                }
-            }
-        })
-    },
+        return res.redirect("/");
+      }
 
-    logout : (req,res)=>{
-        req.session.destroy();
-        return res.redirect('/');
-    },
-
-    register : (req,res)=>{
-        return res.render('users/register')
-    },
-    processRegister : (req,res)=>{
-        const resultValidation = validationResult(req);
-        
-        if(resultValidation.errors.length > 0){
-            return res.render("users/register",{
-                errors: resultValidation.mapped(),
-                oldData: req.body
-            })
-        }
-        const newUsuario = {
-            nombre : req.body.name,
-            apellido : req.body.lastName,
-            email : req.body.email,
-            telefono : req.body.telefono,
-            password : bcrypt.hashSync(req.body.password, 10),
-            genero : req.body.genero,
-            categoria : req.body.persona,
-            confirmarPassword : req.body.confirmarPassword,
-            avatar : req.file.filename
-        }
-        newUsuario.categoria.trim()
-        usersModel.create(newUsuario)
-        res.redirect("/")
+      return res.render("users/login", {
+        errors: {
+          email: {
+            msg: "Las credenciales son invalidas",
+          },
+        },
+      });
     }
-}
+
+    return res.render("users/login", {
+      errors: {
+        email: {
+          msg: "Este correo no esta registrado",
+        },
+      },
+    });
+  },
+
+  logout: (req, res) => {
+    req.session.destroy();
+    res.clearCookie("userEmail");
+    return res.redirect("/");
+  },
+
+  register: (req, res) => {
+    return res.render("users/register");
+  },
+  processRegister: (req, res) => {
+    const resultValidation = validationResult(req);
+
+    if (resultValidation.errors.length > 0) {
+      return res.render("users/register", {
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+      });
+    }
+    const newUsuario = {
+      nombre: req.body.name,
+      apellido: req.body.lastName,
+      email: req.body.email,
+      telefono: req.body.telefono,
+      password: bcrypt.hashSync(req.body.password, 10),
+      genero: req.body.genero,
+      categoria: req.body.persona,
+      confirmarPassword: req.body.confirmarPassword,
+      avatar: req.file.filename,
+    };
+    newUsuario.categoria.trim();
+    usersModel.create(newUsuario);
+    res.redirect("/");
+  },
+};
 
 module.exports = userController;
--- src/middleware/userLoggedMiddleware.js
+++ src/middleware/userLoggedMiddleware.js
@@ -1,11 +1,18 @@
-function userLoggedMiddleware(req,res,next){
-    res.locals.isLogged = false;
-    if(req.session && req.session.userLoged){
-        
-        res.locals.isLogged = true;
-        res.locals.userLoged = req.session.userLoged
-    }
-    next();
+const User = require("../data/users.json");
+
+function userLoggedMiddleware(req, res, next) {
+  res.locals.isLogged = false;
+  let emailCookie = req.cookies.userEmail;
+  let userCookie = User.find((user) => user.email === emailCookie);
+  if (userCookie) {
+    req.session.userLoged = userCookie;
+  }
+  if (req.session && req.session.userLoged) {
+    res.locals.isLogged = true;
+    res.locals.userLoged = req.session.userLoged;
+  }
+
+  next();
 }
 
 module.exports = userLoggedMiddleware;
